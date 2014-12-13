1e1aa80660dbbf050d8c8d022e473c239808c865
Alastair Jones <alastair.peter@gmail.com>
2014-12-13 17:08:41 +0000

    initial blank application

---
+++ RPGM.Notes/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RPGM.Notes")]
+[assembly: AssemblyDescription("A windows phone application for managing roleplay notes.")]
+[assembly: AssemblyProduct("RPGM.Notes")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
+
+// Version information for this assembly.
+[assembly: AssemblyInformationalVersion("0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0")]
+
+// Defines whether this assembly is visible to COM.
+[assembly: ComVisible(false)]
---
+++ RPGM.Notes/ViewModels/Locator.cs
@@ -0,0 +1,43 @@
+using GalaSoft.MvvmLight.Ioc;
+using Microsoft.Practices.ServiceLocation;
+
+namespace RPGM.Notes.ViewModels
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class Locator
+    {
+        /// <summary>
+        /// Initializes a new instance of the Locator class.
+        /// </summary>
+        public Locator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel Main
+        {
+            get { return ServiceLocator.Current.GetInstance<MainViewModel>(); }
+        }
+        
+        public static void Cleanup()
+        {
+            // TODO Clear the ViewModels
+        }
+    }
+}
