fe256fac42beb53799beeba5176cfa5a6ad40bcd
Alastair Jones <alastair.peter@gmail.com>
2015-01-06 23:54:06 +0000

    Revert "alternate link solution"
    This reverts commit 6d0a74dd33571af5eb607d42308f43efb8bd9193.

--- RPGM.Notes/ViewModels/NoteViewModel.cs
+++ RPGM.Notes/ViewModels/NoteViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using GalaSoft.MvvmLight.Command;
@@ -97,10 +96,10 @@ namespace RPGM.Notes.ViewModels
                         // Remove links so we don't save them
                         document.SetText(TextSetOptions.FormatRtf, note.RtfContent);
                     }
-                    else
-                    {
-                        ApplyHyperlinks();
-                    }
+                    //else
+                    //{
+                    //    ApplyHyperlinks();
+                    //}
                 }
             }
         }
@@ -131,22 +130,30 @@ namespace RPGM.Notes.ViewModels
 
         public void ApplyHyperlinks()
         {
+            System.Diagnostics.Debug.WriteLine("ApplyHyperlinks()");
+
             // TODO: Use an alias table
             var notes = Database.ListAsync().Result.Except(new[] { note }).ToArray();
 
             // Avoid performance implications of many small updates
             document.BatchDisplayUpdates();
 
-            string text;
-            document.GetText(TextGetOptions.FormatRtf, out text);
+            ITextRange range;
             foreach (var n in notes)
             {
-                // NOTE: Regex boundary anchors to avoid matching partial words
-                // TODO: Fix boundary anchors excluding notes with with non-alphanumeric edge characters
-                text = Regex.Replace(text, "\\b(" + Regex.Escape(n.Title) + ")\\b", "{\\field{\\*\\fldinst HYPERLINK \"richtea.rpgm://notes/" + n.Id + "\"}{\\fldrslt $1}}", RegexOptions.IgnoreCase | RegexOptions.RightToLeft);
+                var link = string.Format("\"richtea.rpgm://notes/{0}\"", n.Id);
+                var skip = 0;
+
+                while ((range = document.GetRange(skip, TextConstants.MaxUnitCount)).FindText(n.Title, TextConstants.MaxUnitCount, FindOptions.None) != 0)
+                {
+                    System.Diagnostics.Debug.WriteLine("Setting text at position {0} to link: '{1}'.", range.StartPosition, link);
+
+                    // TODO: Stop this throw exceptions
+                    range.Link = link;
+                    skip = range.EndPosition;
+                }
             }
 
-            document.SetText(TextSetOptions.FormatRtf, text);
             document.ApplyDisplayUpdates();
         }
 
@@ -204,6 +211,7 @@ namespace RPGM.Notes.ViewModels
             // Restore note to original as it likely has changes
             note = new Note(original);
             document.SetText(TextSetOptions.FormatRtf, note.RtfContent);
+            //TryInitializeDocument();
             RaisePropertyChanged("Title");
             IsEditMode = false;
         }
@@ -230,6 +238,9 @@ namespace RPGM.Notes.ViewModels
                 string rtfContent;
                 document.GetText(TextGetOptions.FormatRtf, out rtfContent);
                 note.RtfContent = rtfContent;
+
+                System.Diagnostics.Debug.WriteLine("Saving RTF:");
+                System.Diagnostics.Debug.WriteLine(note.RtfContent);
             }
 
             await Database.SaveAsync(note);
