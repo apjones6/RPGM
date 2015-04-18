430ebbcbe4b7af2cf24b32bac84043378f3c623c
Alastair Jones <alastair.peter@gmail.com>
2015-04-18 10:32:40 +0100

    empty list message

---
+++ RPGM.Notes/Converters/IntToInvisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace RPGM.Notes.Converters
+{
+    public class IntToInvisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value != null && value.Equals(0) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // Throw as there's no way we can safely convert false to the original value
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ RPGM.Notes/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace RPGM.Notes.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value != null && value.Equals(0) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            // Throw as there's no way we can safely convert true to the original value
+            throw new NotSupportedException();
+        }
+    }
+}
